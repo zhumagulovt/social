ab17fc5d5dfaa6b86b7317352fa97187f58dc13a
zhumagulov torokeldi <zhumagulovtorokeldi@gmail.com>
2022-09-17 21:29:55 +0600

    add create post tests

--- posts/tests.py
+++ posts/tests.py
@@ -1,1 +1,34 @@
-from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APITestCase, APIRequestFactory, force_authenticate, APIClient
+
+from django.contrib.auth import get_user_model
+
+from .views import FollowingFeedView
+from .models import Post
+
+from accounts.models import UserFollowing
+
+User = get_user_model()
+
+
+class PostCreateTest(APITestCase):
+
+    def setUp(self):
+        user = User.objects.create_user(
+            username='test_user',
+            email='test_user@test.com',
+            password='Qwerty123'
+        )
+        user.is_active = True
+        user.save()
+
+    def test_create_post(self):
+        user = User.objects.get(pk=1)
+        
+        self.client.force_authenticate(user=user)
+        response = self.client.post('/api/v1/posts/', data={
+            'content': 'This is a test post'
+        })
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Post.objects.count(), 1)
--- posts/urls.py
+++ posts/urls.py
@@ -3,10 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.FollowingFeedView.as_view()),
+    path('', views.PostCreateView.as_view()),
+    path('feed/', views.FollowingFeedView.as_view()),
     path('<int:pk>/', views.PostDetailDeleteView.as_view()),
     path('<int:pk>/add_comment/', views.CommentCreateView.as_view()),
     path('<int:pk>/like/', views.LikeView.as_view()),
-    path('create/', views.PostCreateView.as_view()),
     path('comment/<int:pk>/', views.CommentDeleteView.as_view())
 ]
