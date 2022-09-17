deb476f9abb499ed72c4b3b62710c6f5357ce41f
zhumagulov torokeldi <zhumagulovtorokeldi@gmail.com>
2022-09-17 19:26:57 +0600

    add login test

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,30 @@
-from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class UserAuthTests(APITestCase):
+
+    def setUp(self):
+        user = User.objects.create_user(
+            username='test_user',
+            email='test_user@gmail.com',
+            password='Qwerty123'
+        )
+        user.is_active = True
+        user.save()
+
+    
+    def test_login(self):
+
+        url = "/api/v1/accounts/login/"
+        data = {'username': 'test_user', 'password': 'Qwerty123'}
+
+        response = self.client.post(url, data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn('access', response.data)
+        self.assertIn('refresh', response.data)
--- posts/urls.py
+++ posts/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
     path('', views.FollowingFeedView.as_view()),
     path('<int:pk>/', views.PostDetailDeleteView.as_view()),
     path('<int:pk>/add_comment/', views.CommentCreateView.as_view()),
