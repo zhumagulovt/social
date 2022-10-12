32793a4eb9207b61f943d69aa363d03d13c61852
zhumagulov torokeldi <zhumagulovtorokeldi@gmail.com>
2022-10-12 22:22:59 +0600

    add tests for like and comments

--- posts/tests.py
+++ posts/tests.py
@@ -3,7 +3,7 @@ from rest_framework.test import APITestCase
 
 from django.contrib.auth import get_user_model
 
-from .models import Comment, Post
+from .models import Comment, Like, Post
 
 User = get_user_model()
 
@@ -48,3 +48,42 @@ class CommentCreateTest(APITestCase):
 
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(Comment.objects.count(), 1)
+        
+        comment = Comment.objects.all().first()
+        post = Post.objects.get(pk=1)
+        self.assertEqual(post.comments.all().first(), comment)
+
+
+class LikeTest(APITestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username="test_user", email="test_user@test.com", password="Qwerty123"
+        )
+        self.user.is_active = True
+        self.user.save()
+
+        self.post = Post.objects.create(content='This is a test post', user=self.user)
+
+    def test_post_like(self):
+
+        self.client.force_authenticate(user=self.user)
+        response = self.client.post(
+            "/api/v1/posts/1/like/"
+        )
+        
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Like.objects.count(), 1)
+        self.assertEqual(self.post.likes.count(), 1)
+    
+    def test_post_unlike(self):
+        Like.objects.create(post=self.post, user=self.user)
+
+        self.client.force_authenticate(user=self.user)
+        response = self.client.post(
+            "/api/v1/posts/1/like/"
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Like.objects.count(), 0)
+        self.assertEqual(self.post.likes.count(), 0)
